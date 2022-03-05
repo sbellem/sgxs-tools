ff077f6283ae23a90b00b04f85b86cd058db8687
Sylvain Bellemare <sbellem@gmail.com>
2022-03-05 01:03:42 -0600

    Add devShell

--- flake.nix
+++ flake.nix
@@ -4,42 +4,68 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-      in
-      with pkgs;
-      {
-        defaultPackage = rustPlatform.buildRustPackage rec {
-          pname = "sgxs-tools";
-          version = "0.8.5";
 
-          src = fetchCrate {
-            inherit pname version;
-            sha256 = "sha256-zKzZOpdO3H1/4tKSdHLbyhY0iAuLoscEfgFDHsQbK3w=";
+        _nativeBuildInputs = with pkgs; [
+          clang_11
+          llvmPackages_11.libclang.lib
+          pkg-config
+          protobuf
+          rust-bin.nightly."2021-11-04".default
+        ];
+
+        _buildInputs = with pkgs; [
+          openssl.dev
+        ];
+      in
+        with pkgs; {
+          defaultPackage = rustPlatform.buildRustPackage rec {
+            inherit _buildInputs _nativeBuildInputs;
+            pname = "sgxs-tools";
+            version = "0.8.5";
+
+            src = fetchCrate {
+              inherit pname version;
+              sha256 = "sha256-zKzZOpdO3H1/4tKSdHLbyhY0iAuLoscEfgFDHsQbK3w=";
+            };
+
+            cargoSha256 = "sha256-8PxIL/ZaJIocDlYD2fTZ6M2QYkssUq1LqlEHlOUO+U8=";
+
+            nativeBuildInputs = _nativeBuildInputs;
+            buildInputs = _buildInputs;
           };
 
-          cargoSha256 = "sha256-8PxIL/ZaJIocDlYD2fTZ6M2QYkssUq1LqlEHlOUO+U8=";
-
-          nativeBuildInputs = [
-            clang_11
-            llvmPackages_11.libclang.lib
-            pkg-config
-            protobuf
-            rust-bin.nightly."2021-11-04".default
-          ];
-
-          buildInputs = [
-            openssl.dev
-          ];
-        };
-      }
+          devShell = mkShell {
+            inherit _buildInputs _nativeBuildInputs;
+            buildInputs =
+              _buildInputs
+              ++ _nativeBuildInputs
+              ++ [
+                pkgs.exa
+                pkgs.unixtools.whereis
+                pkgs.which
+              ];
+            shellHook = ''
+              alias ls=exa
+              alias find=fd
+              export RUST_BACKTRACE=1
+            '';
+          };
+        }
     );
 }
